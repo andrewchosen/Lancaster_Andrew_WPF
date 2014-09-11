0972638e33d66990063e50c96ebf8734afdfa540
andrewchosen <andrewlancaster@andrews-mbp.home>
2014-09-11 15:51:59 -0700

    Created project
    Created first version of project.

--- Wacky/js/script.js
+++ Wacky/js/script.js
@@ -1,3 +1,24 @@
-/**
- * Created by andrewlancaster on 9/10/14.
- */
+//Assignment: Expressions
+//Wacky
+//by Andrew Lancaster
+//created on 9/11/2014
+
+//Finding optimal column width depending on screen width and padding.
+
+var height = prompt("Karate chop your brick here. \n\nHeight of brick in inches.")
+var length = prompt("Length of brick in inches.");
+var depth = prompt("Depth of brick in inches.");
+var chops = prompt("How many times do you want to chop it into equally sized pieces?");
+
+var volume = height * length * depth;
+var pieces = chops * 2;
+var result = volume / pieces;
+
+if(chops == 0){
+console.log("You chose not to chop your " + volume + " square inch brick. That is no fun at all. Please refresh and CHOP THE BRICK!")
+}
+else{
+console.log("After chopping the " + volume + " square inch brick " + chops + " time(s), you are left with " + pieces + " equal " + result + " square inch bricks.")
+}
+
+	
