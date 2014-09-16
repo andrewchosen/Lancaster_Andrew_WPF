c709a1900f66180743d712c06dbe114ac1c5beed
andrewchosen <andrew.chosen@gmail.com>
2014-09-16 11:59:15 -0700

    Added comments
    Commented lines

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,49 +2,63 @@
  * Personal
  * Created by Andrew Lancaster on 9/16/2014 */
 
-//Prompts: Gathering fiance(guy) and fiancee(girl) monthly income and expenses
-var monthsTilWedding = prompt("This calculation will determine if you will be able to afford to pay for your own wedding or if you'll need help. \n\nHow many months until your wedding?")
-if(monthsTilWedding === ""){
-	monthsTilWedding = prompt("Oops, you forgot to answer the question. Please try again. \n\nHow many months until your wedding?")
+/* Topic: Can we afford our wedding? */ 
+
+//Question #1: "How many months until your wedding?"
+var monthsTilWedding = prompt("This calculation will determine if you will be able to afford to pay for your own wedding or if you'll need help. \n\nHow many months until your wedding?");
+if(monthsTilWedding === ""){ //If answer is empty
+	monthsTilWedding = prompt("Oops, you forgot to answer the question. Please try again. \n\nHow many months until your wedding?"); //show error, then ask question again
 }
 
-var moneyNeeded = prompt("How much money do you need to save by then?")
-if(moneyNeeded === ""){
-	moneyNeeded = prompt("Oops, you forgot to answer the question. Please try again. \n\nHow much money do you need to save by then?")
+//Question #2: "How much money do you need to save by then?"
+var moneyNeeded = prompt("How much money do you need to save by then?");
+if(moneyNeeded === ""){ //If answer is empty
+	moneyNeeded = prompt("Oops, you forgot to answer the question. Please try again. \n\nHow much money do you need to save by then?"); //show error, then ask question again
 }
 
-var fianceIncome = prompt("What is the groom's monthly gross income?")
-if(fianceIncome === ""){
-	fianceIncome = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the groom's monthly gross income?")
+//Question #3: "What is the groom's monthly gross income?"
+var fianceIncome = prompt("What is the groom's monthly gross income?");
+if(fianceIncome === ""){ //If answer is empty
+	fianceIncome = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the groom's monthly gross income?"); //show error, then ask question again
 }
 
-var fianceExpenses = prompt("What are his total monthly expenses?")
-if(fianceExpenses === ""){
-	fianceExpenses = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat are his total monthly expenses?")
+//Question #4: "What are his total monthly expenses?"
+var fianceExpenses = prompt("What are his total monthly expenses?");
+if(fianceExpenses === ""){ //If answer is empty
+	fianceExpenses = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat are his total monthly expenses?"); //show error, then ask question again
 }
 
-var fianceeIncome = prompt("What is the bride's monthly gross income?")
-if(fianceeIncome === ""){
-	fianceeIncome = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the bride's monthly gross income?")
+//Question: #5: "What is the bride's monthly gross income?"
+var fianceeIncome = prompt("What is the bride's monthly gross income?");
+if(fianceeIncome === ""){ //If answer is empty
+	fianceeIncome = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the bride's monthly gross income?"); //show error, then ask question again
 }
 
-var fianceeExpenses = prompt("What are her total monthly expenses?")
-if(fianceeExpenses === ""){
-	fianceeExpenses = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat are her total monthly expenses?")
+//Question #6: "What are her total monthly expenses?"
+var fianceeExpenses = prompt("What are her total monthly expenses?");
+if(fianceeExpenses === ""){ //If answer is empty
+	fianceeExpenses = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat are her total monthly expenses?"); //show error, then ask question again
 }
 
-var combinedIncome = (fianceIncome - fianceExpenses) + (fianceeIncome - fianceeExpenses)
-var maxSavings = combinedIncome * monthsTilWedding
+var combinedSavings = (fianceIncome - fianceExpenses) + (fianceeIncome - fianceeExpenses); //subtracting each gross income from expenses and combining them for a total monthly savings
+var maxSavings = combinedSavings * monthsTilWedding //converting total monthly savings into total savings over the time until the wedding
+var monthlySavingsNeeded = moneyNeeded / monthsTilWedding; //find how much money is needed to be saved per month
+var result = maxSavings - moneyNeeded //calculate the amount left over
 
-var monthlySavings = moneyNeeded / monthsTilWedding;
-var result = maxSavings - moneyNeeded
-
-if(result <= 0){
+//If amount leftover is less than zero
+if(result < 0){
+	//Output that the couple cannot afford the wedding and display result as positive
 	console.log("You cannot afford this wedding by yourselves. You will need at least $" + -result + " from other sources to cover the amount you need, and that is without allowing yourselves breathing room.");
+//If amount leftover is 0-200
 }else if(result <=200){
+	//Explain that the couple can afford the wedding, but just barely
 	console.log("You can just barely afford the wedding with only $" + result + " left over. It is recommended that you look to cut some expenses to allow for more breathing room.");
+//If amount leftover is 1,000 or more
 }else if(result >=1000){
-	console.log("You can afford your wedding! You both have well over the amount in extra funds to save for the wedding with $" + result + " of breathing room. Just remember to save at least $" + monthlySavings + "/month to reach your goal.");
+	//Explain that the couple is in good shape to pay for their own wedding
+	console.log("You can afford your wedding! You both have well over the amount in extra funds to save for the wedding with $" + result + " of breathing room. Just remember to save at least $" + monthlySavingsNeeded + "/month to reach your goal.");
+//If amount leftover is 201-999
 }else{
-	console.log("You can afford your wedding with $" + result + " of breathing room! Just remember to save at least $" + monthlySavings + "/month to reach your goal.");
+	//Explain that the couple can afford their wedding with a warning to save enough per month
+	console.log("You can afford your wedding with $" + result + " of breathing room! Just remember to save at least $" + monthlySavingsNeeded + "/month to reach your goal.");
 }
