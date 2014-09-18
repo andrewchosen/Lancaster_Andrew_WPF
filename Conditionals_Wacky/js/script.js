1d28021596081d6d83a2d440389d608ad33aa049
andrewchosen <andrewlancaster@andrews-mbp.home>
2014-09-18 16:25:44 -0700

    Added IF statements and comments
    Added IF statement for empty answers with comments

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,10 +5,29 @@
 
 //Finding optimal column width depending on screen width and padding.
 
-var height = prompt("Karate chop your brick here. \n\nHeight of brick in inches.")
+//Find the height of the brick
+var height = prompt("Karate chop your brick into equal pieces here. \n\nHeight of brick in inches.")
+if(height === ""){ //If answer is empty
+    height = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the height of brick in inches?"); //show error, then ask question again
+}
+
+//Find the length of the brick
 var length = prompt("Length of brick in inches.");
+if(length === ""){ //If answer is empty
+    length = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the length of brick in inches?"); //show error, then ask question again
+}
+
+//Find the depth of the brick
 var depth = prompt("Depth of brick in inches.");
+if(depth === ""){ //If answer is empty
+    depth = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the depth of brick in inches?"); //show error, then ask question again
+}
+
+//Find out how many times user will chop the brick
 var chops = prompt("How many times do you want to chop it into equally sized pieces?");
+if(chops === ""){ //If answer is empty
+    chops = prompt("Oops, you forgot to answer the question. Please try again. \n\nWhat is the chops of brick in inches?"); //show error, then ask question again
+}
 
 var volume = height * length * depth;
 var pieces = chops * 2;
