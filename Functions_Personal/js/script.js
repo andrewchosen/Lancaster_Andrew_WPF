a0c4ce004d9bcb8f074e9fd461b36d3822ca8f66
andrewchosen <andrewlancaster@andrews-mbp.home>
2014-09-25 14:35:16 -0700

    Functions - Personal
    First draft of script.js

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,17 @@
 /*  Function Assignment:
  *  Personal
  *  Created by Andrew Lancaster on September 24, 2014 */
+
+var totalGuests = prompt('How much money will each wedding guest need to give us to meet our goal amount?\n\nHow many guests will be at the wedding?');
+var nonGifters = prompt('How many of the guests are children or people who most likely wont be able to give separately?');
+var amountNeeded = prompt('What is your goal amount needed?')
+var gifters = totalGuests - nonGifters;
+
+var calcResult = function(g, a){
+    var result = a / g;
+    return result;
+}
+
+var a = calcResult(gifters, amountNeeded);
+
+console.log('You have a total of ' + gifters + ' guests who will most likely give a gift. To reach your goal of $' + amountNeeded + ', each guest will need to give $' + a + '.');
