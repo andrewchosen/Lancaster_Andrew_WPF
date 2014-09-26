842dba08fa145b776cbb97918d554b32ffdd6cab
andrewchosen <andrewlancaster@andrews-mbp.home>
2014-09-25 17:19:32 -0700

    Test #1
    Testing javascript function concept

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,11 @@
  *  Industry
  *  Created by Andrew Lancaster on September 24, 2014 */
 
+var siteOneCost = 0;
+var siteTwoCost = 10;
+var siteThreeCost = 10;
+var siteFourCost = 10;
+
+function calcAverage(siteOneCost, siteTwoCost, siteThreeCost, siteFourCost){
+    var calcAverage = (siteOneCost + siteTwoCost + siteThreeCost + siteFourCost) / 4;
+}
