6d435976a8d48a4218939bde796b83c4b0f5cefb
andrewchosen <andrewlancaster@andrews-mbp.home>
2014-09-25 17:34:03 -0700

    Prompts
    Added user prompts

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,6 +2,11 @@
  *  Industry
  *  Created by Andrew Lancaster on September 24, 2014 */
 
+var siteOneCost = prompt('Based on the average earnings of your last four website designs, will you reach your goal at 50 websites?\n\nWhat was the earnings of your last website design?');
+var siteTwoCost = prompt('How about the one before that(#2)?');
+var siteThreeCost = prompt('And the one before that(#3)?');
+var siteFourCost = prompt('And the one before that(#4)?');
+var goal = prompt('What is the least amount you want to make after 50 sites (including the previous four)?')
 
 function calcAverage(one, two, three, four){
     var calcAverage = (one + two + three + four) / 4;
